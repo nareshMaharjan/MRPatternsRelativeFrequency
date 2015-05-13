3cce36775c3b58b1bf34b51eba5726679e0227c2
Naresh Maharjan <naresh.mjan@gmail.com>
2015-05-13 15:14:22 -0500

    in-mapper combiner

--- src/main/java/com/hd/crystalBall/RelFreqPairMapper.java
+++ src/main/java/com/hd/crystalBall/RelFreqPairMapper.java
@@ -1,12 +1,15 @@
 package com.hd.crystalBall;
 
 import com.hd.utils.NumberPair;
+import com.hd.utils.StripeMapWritableCustom;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,9 +19,11 @@ import java.io.IOException;
  * To change this template use File | Settings | File Templates.
  */
 public class RelFreqPairMapper extends Mapper<LongWritable, Text, NumberPair, IntWritable> {
+    HashMap<NumberPair,Integer> H;
+
     @Override
     public void setup(Context context){
-
+        H =new HashMap<NumberPair,Integer>();
     }
 
     @Override
@@ -34,15 +39,28 @@ public class RelFreqPairMapper extends Mapper<LongWritable, Text, NumberPair, In
                 NumberPair tp = new NumberPair(terms[i],terms[j]);
                 NumberPair tpt = new NumberPair(terms[i],"*");
                 System.out.println("tp:: "+tp+"   tpt::  "+tpt);
-                context.write(tpt,new IntWritable(1));
-                context.write(tp,new IntWritable(1));
+                addElement(tp);
+                addElement(tpt);
+//                context.write(tpt,new IntWritable(1));
+//                context.write(tp,new IntWritable(1));
             }
         }
 
     }
 
     @Override
-    public void cleanup(Context context){
+    public void cleanup(Context context) throws IOException, InterruptedException {
         System.out.println("The mapper clean up");
+        for(Map.Entry<NumberPair,Integer> m: H.entrySet()){
+            context.write(m.getKey(),new IntWritable(m.getValue()));
+        }
+    }
+
+    public void addElement(NumberPair n){
+        if (H.containsKey(n)) {
+            H.put(n, H.get(n) + 1);
+        } else {
+            H.put(n, 1);
+        }
     }
 }
